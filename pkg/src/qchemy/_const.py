b481f36ef29b09a21e327528d4a64aa9e95e2812
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-11-11 06:16:56 +0100

    Update _const.py

--- pkg/src/qchemy/_const.py
+++ pkg/src/qchemy/_const.py
@@ -1,13 +1,15 @@
 """Constants used in Qchemy."""
 
 
-L_TO_LABEL = {0: "s", 1: "p", 2: "d", 3: "f", 4: "g", 5: "h", 6: "i"} | dict(
+L_TO_LABEL: dict[int, str] = {
+    0: "s", 1: "p", 2: "d", 3: "f", 4: "g", 5: "h", 6: "i"
+} | dict(
     zip(range(7, 7 + (ord('z') - ord('k') + 1)), map(chr, range(ord('k'), ord('z') + 1)))
 )
 """Mapping from azimuthal quantum number `l` to subshell label."""
 
 
-LABEL_TO_L = {v: k for k, v in L_TO_LABEL.items()}
+LABEL_TO_L: dict[str, int] = {v: k for k, v in L_TO_LABEL.items()}
 """Mapping from subshell label to azimuthal quantum number `l`."""
 
 
